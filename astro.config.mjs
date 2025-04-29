8f0d8e82d471d520ac42cd66e442989da06bff08
Daniel Rojas <l20j45_@gmail.com>
2025-04-29 15:50:23 -0600

    Termine loggin y registrar, voy a hacer el perfil de los alumnos

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import purgecss from 'astro-purgecss';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  compressHTML: false,
+  integrations: [purgecss()],
+  vite: {
+    server: {
+      allowedHosts: true,
+    },
+    preview: {
+      allowedHosts: true,
+    }
+  },
+});
